10ea9225ab3c96582e4f6832389262dec36d87b2
leonardo-ggomes <131769868+leonardo-ggomes@users.noreply.github.com>
2023-09-26 20:05:56 -0300

    Atualização Script.js
    Ajusted svg.contentDocument

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    document.getElementById("cpSleeve").addEventListener("input", (colorpicker) => {
-        let chosenColor = colorpicker.target.value
-        let svgObject = document.querySelector("#svgObject").contentDocument
-        let shirtSleeve = svgObject.querySelectorAll(".shirt-sleeve")
-    
-        shirtSleeve.forEach(sleeve => sleeve.setAttribute("fill",chosenColor));
-    })
+    const svgObject = document.getElementById("svgObject")  
 
-    document.getElementById("cpShirt").addEventListener("input", (colorpicker) => {
-        let chosenColor = colorpicker.target.value
-        let svgObject = document.querySelector("#svgObject").contentDocument
-        let shirtBody = svgObject.querySelectorAll(".shirt-body")
-    
-        shirtBody.forEach(body => body.setAttribute("fill",chosenColor));
-    })
+    svgObject.addEventListener("load", () => {
+        let svgDoc = svgObject.contentDocument;
 
+        document.getElementById("cpSleeve").addEventListener("input", (colorpicker) => {
+            let chosenColor = colorpicker.target.value
+            let shirtSleeve = svgDoc.querySelectorAll(".shirt-sleeve")
+        
+            shirtSleeve.forEach(sleeve => sleeve.setAttribute("fill",chosenColor));
+        })
+    
+        document.getElementById("cpShirt").addEventListener("input", (colorpicker) => {
+            let chosenColor = colorpicker.target.value
+            let shirtBody = svgDoc.querySelectorAll(".shirt-body")
+        
+            shirtBody.forEach(body => body.setAttribute("fill",chosenColor));
+        })
+
+    })
 })
 
